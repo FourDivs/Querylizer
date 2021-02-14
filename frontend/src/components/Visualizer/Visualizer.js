d470a3e878dd695a56442d681e74ea961a8e14e2
kothariji <kotharidhruv25@gmail.com>
2021-02-14 13:04:10 +0530

    dynamic node addition

--- frontend/src/components/Visualizer/Visualizer.js
+++ frontend/src/components/Visualizer/Visualizer.js
@@ -1,16 +1,19 @@
 import Diagram, { createSchema, useSchema } from 'beautiful-react-diagrams';
 
 // the diagram model
+
+
+
 const initialSchema = createSchema({
   nodes: [
-    { id: 'node-1', content: 'Node 1', coordinates: [250, 60], },
-    { id: 'node-2', content: 'Node 2', coordinates: [100, 200], },
-    { id: 'node-3', content: 'abcd', coordinates: [50, 220], },
-    { id: 'node-4', content: 'Node 4', coordinates: [400, 200], },
+    { id: 'node-1', content: 'Table', coordinates: [250, 60], },
+    { id: 'node-2', content: 'row', coordinates: [100, 200], },
+    { id: 'node-3', content: 'row', coordinates: [250, 220], },
+    { id: 'node-4', content: 'row', coordinates: [400, 200], },
   ],
   links: [
     { input: 'node-1',  output: 'node-2' },
-    { input: 'node-3',  output: 'node-2' },
+    { input: 'node-1',  output: 'node-3' },
     { input: 'node-1',  output: 'node-4' },
   ]
 });
@@ -18,10 +21,27 @@ const initialSchema = createSchema({
 
 const Visualizer = () => {
   // create diagrams schema
-  const [schema, { onChange }] = useSchema(initialSchema);
+  const [schema, { onChange, addNode, removeNode }] = useSchema(initialSchema);
+
+
+
+  const addNewNode = () => {
+    const nextNode = {
+      id: `node-${schema.nodes.length+1}`,
+        content: `Node ${schema.nodes.length+1}`,
+        coordinates: [
+          schema.nodes[schema.nodes.length - 1].coordinates[0] + 100,
+          schema.nodes[schema.nodes.length - 1].coordinates[1],
+        ],
+    };
+    addNode(nextNode);
+  }
+
+
 
   return (
     <div style={{ height: '22.5rem' }}>
+      <button color="primary" icon="plus" onClick={addNewNode}>Add new node</button>
       <Diagram schema={schema} onChange={onChange} />
     </div>
   );
