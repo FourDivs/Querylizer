0001ec476c401e911a42ad86e192dd6e60a82325
kothariji <kotharidhruv25@gmail.com>
2021-03-05 17:05:48 +0530

    :rocket:[Feat]: Suspense lazy load
    added lazy laoding to the app routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
-import { useState } from "react";
+import { useState, Suspense, lazy } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Visualizer from "./components/Visualizer/Visualizer"
-import Home from "./components/Home/Home"
 import { UserContext } from "./context/UserContext";
-
 
 //firebase setup and initialzation
 import firebase from "firebase/app"
 import "firebase/auth"
 import firebaseConfig from "./config/firebaseConfig";
 firebase.initializeApp(firebaseConfig);
+
+
+//lazy loading
+const Home = lazy(() => import("./components/Home/Home"));
+const Visualizer = lazy(() => import('./components/Visualizer/Visualizer'));
 
 
 const App = () => {
@@ -19,11 +21,14 @@ const App = () => {
   return (
     <Router>
       <UserContext.Provider value = {{user, setUser}}>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/visualizer" component={Visualizer} />
-          {/* TODO: <Route exact path="*" component={PageNotFound} /> */}
-        </Switch>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/visualizer" component={Visualizer} />
+            {/* TODO: <Route exact path="/aboutus" component={AboutUs} /> */}
+            {/* TODO: <Route exact path="*" component={PageNotFound} /> */}
+          </Switch>
+        </Suspense>
       </UserContext.Provider>
     </Router>
   );
