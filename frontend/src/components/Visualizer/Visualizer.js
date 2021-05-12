e049805d991bcda76d0548731c9b7b0be0a9c440
Akash-Salvi <akashnov100@gmail.com>
2021-05-12 23:50:34 +0530

    :hammer:[Bug]: Clear Screen Icon Changed

--- frontend/src/components/Visualizer/Visualizer.js
+++ frontend/src/components/Visualizer/Visualizer.js
@@ -9,7 +9,7 @@ import { useScreenshot } from 'use-react-screenshot';
 import AddBoxIcon from '@material-ui/icons/AddBox';
 import AddToPhotosIcon from '@material-ui/icons/AddToPhotos';
 import CodeIcon from '@material-ui/icons/Code';
-import SaveIcon from '@material-ui/icons/Save';
+import DeleteIcon from '@material-ui/icons/Delete';
 import {
   FormControl,
   MenuItem,
@@ -520,7 +520,7 @@ const Visualizer = () => {
         <MUIButton onClick = {addTableNode} startIcon={<AddBoxIcon />} variant="contained" style = {{backgroundColor: "#4cd137", color: '#fff' , top:"69%",right:"1%",zIndex:"1",position:"absolute"}}> <Typography style = {{fontFamily: 'Poppins', fontWeight: "600"}}>Add Table </Typography></MUIButton>
         <MUIButton onClick = {addNewNode} startIcon={<AddToPhotosIcon />} variant="contained" style = {{backgroundColor: "#7158e2", color: '#fff' , top:"76%",right:"1%",zIndex:"1",position:"absolute"}}> <Typography style = {{fontFamily: 'Poppins', fontWeight: "600"}}>Add Field </Typography></MUIButton>
         <MUIButton onClick = {handleSubmit} startIcon={<CodeIcon />} variant="contained" style = {{backgroundColor: "#fbc531", color: '#fff' , top:"83%",right:"1%",zIndex:"1",position:"absolute"}}> <Typography style = {{fontFamily: 'Poppins', fontWeight: "600"}}>Generate Code </Typography></MUIButton>
-        <MUIButton onClick = {clearDiagram} startIcon={<SaveIcon />} variant="contained" style = {{backgroundColor: "#ff4d4d", color: '#fff' , top:"90%",right:"1%",zIndex:"1",position:"absolute"}}> <Typography style = {{fontFamily: 'Poppins', fontWeight: "600"}}>Clear Screen</Typography></MUIButton>
+        <MUIButton onClick = {clearDiagram} startIcon={<DeleteIcon />} variant="contained" style = {{backgroundColor: "#ff4d4d", color: '#fff' , top:"90%",right:"1%",zIndex:"1",position:"absolute"}}> <Typography style = {{fontFamily: 'Poppins', fontWeight: "600"}}>Clear Screen</Typography></MUIButton>
       </div>
     </Fragment>
   );
